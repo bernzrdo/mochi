cd3c21db2a67c6b9fbce1955088d03d20fa137bb
bernardo silva <bernardovs2003@gmail.com>
2023-10-17 18:31:11 +0100

    criar README

---
+++ README.md
@@ -0,0 +1,12 @@
+# <img src="https://i.imgur.com/N74BwjO.png" style="height: 1.2em; vertical-align: -20%;"> Mochi
+
+O Mochi é um bot de Discord que adiciona comandos úteis a qualquer servidor! O que lhe distingue de outros bots, não é só ser 100% português de portugal, mas também a sua simplicidade.
+
+|Comando|Descrição|Código|
+|-|-|-|
+|**/calcular**|Calcula uma expressão matemática|[calcular.ts](https://github.com/bernzrdo/mochi/blob/main/commands/calcular.ts)
+|**/countdown**|Mostra quanto tempo falta para uma data|[countdown.ts](https://github.com/bernzrdo/mochi/blob/main/commands/countdown.ts)
+|**/define**|Descobre a definição de uma palavra|[define.ts](https://github.com/bernzrdo/mochi/blob/main/commands/define.ts)
+|**/foto**|Obtem a foto de perfil de algúem|[foto.ts](https://github.com/bernzrdo/mochi/blob/main/commands/foto.ts)
+|**/lofi**|Toco lofi hip hop no canal onde estás|[lofi.ts](https://github.com/bernzrdo/mochi/blob/main/commands/lofi.ts)
+|**/pokemon**|Sabe mais sobre um Pokémon|[pokemon.ts](https://github.com/bernzrdo/mochi/blob/main/commands/pokemon.ts)
--- deploy-commands.ts
+++ deploy-commands.ts
@@ -1,14 +1,14 @@
-import { REST, Routes } from 'discord.js';
+import { REST, RESTPostAPIChatInputApplicationCommandsJSONBody as CommandJSON, Routes } from 'discord.js';
 import { Command } from './global';
-import { readdirSync } from 'fs';
+import { readdirSync, writeFileSync } from 'fs';
 import 'dotenv/config';
 
 const rest = new REST().setToken(process.env.TOKEN!);
 
 (async ()=>{
 
-    let globalCommands: any[] = [];
-    let experimentalCommands: any[] = [];
+    let globalCommands: CommandJSON[] = [];
+    let experimentalCommands: CommandJSON[] = [];
 
     for(let file of readdirSync(__dirname + '/commands')){
 
@@ -32,12 +32,29 @@ const rest = new REST().setToken(process.env.TOKEN!);
         Routes.applicationCommands(process.env.CLIENT_ID!),
         { body: globalCommands }
     );
-        
+
     console.log('Publishing experimental commands...');
     await rest.put(
         Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.TEST_GUILD_ID!),
         { body: experimentalCommands }
     );
+    
+    console.log('Updating README...');
+    
+    let readme = [
+        '# <img src="https://i.imgur.com/N74BwjO.png" style="height: 1.2em; vertical-align: -20%;"> Mochi',
+        '',
+        'O Mochi é um bot de Discord que adiciona comandos úteis a qualquer servidor! O que lhe distingue de outros bots, não é só ser 100% português de portugal, mas também a sua simplicidade.',
+        '',
+        '|Comando|Descrição|Código|',
+        '|-|-|-|'
+    ];
+
+    for(let command of globalCommands){
+        readme.push(`|**/${command.name}**|${command.description}|[${command.name}.ts](https://github.com/bernzrdo/mochi/blob/main/commands/${command.name}.ts)`);
+    }
+
+    writeFileSync('README.md', readme.join('\n'));
 
     console.log('Done!');
     process.exit();
